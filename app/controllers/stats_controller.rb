1e3f077383040dee6f4c141a404dcef5f82ea313
Doug Sparling <doug@cyberdeck.dev>
2021-01-24 16:44:32 -0500

    cleanup landing and prepare for charting.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,26 +5,41 @@ class StatsController < ApplicationController
   def show
     @user = RSpotify::User.new(session[:user])
 
-    @by_year = Hash.new(0)
+    by_year = query_counts_by_year
+    oldest, newest = by_year.keys.minmax
+
+    @year_labels = oldest..newest
+    @year_values = @year_labels.map { |year| by_year[year] || 0 }
+  end
+
+  def query_counts_by_year
+    by_year = Hash.new(0)
+    years = get_saved_tracks.inject([]) do |memo, track|
+      begin
+        memo << Date.parse(track.album.release_date).year
+      rescue Date::Error => e
+        # do nothing
+      end
+      memo
+    end
+
+    years.each do |year|
+      by_year[year] += 1 
+    end
+
+    by_year
+  end
+
+  def get_saved_tracks
     offset = 0
     limit = 20
-    while offset + limit < 1000
+    all = []
+    while offset + limit < 60
       tracks = @user.saved_tracks(offset: offset, limit: limit)
       break if tracks.empty? || tracks.size < limit
-
-      years = tracks.map do |t|
-        begin
-          Date.parse(t.album.release_date).year
-        rescue Date::Error => e
-          0
-        end
-      end
-
-      years.each do |year|
-        @by_year[year] += 1 
-      end
-
+      all.concat(tracks)
       offset += limit
     end
+    return all
   end
 end
