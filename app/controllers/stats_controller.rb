f6cd01a9318a917b16c6ecfd01c43fbde7757ab1
Doug Sparling <doug@cyberdeck.dev>
2021-01-31 19:02:09 -0500

    analyze specific playlists

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,20 +1,46 @@
 class StatsController < ApplicationController
+
+  before_action :authenticate, except: 'index'
+
+  def authenticate
+    user_hash = session[:user]
+    return redirect_to root_url unless user_hash
+    @user = RSpotify::User.new(user_hash)
+  end
+
   def index
   end
 
   def show
-    @user = RSpotify::User.new(session[:user])
+    @playlists = @user.playlists
+  end
 
-    by_year = query_counts_by_year
+  def show_saved_tracks
+    analyze_tracks(@user.method(:saved_tracks))
+    @name = 'My Saved Tracks'
+    render :show_stats
+  end
+
+  def show_playlist
+    playlist = RSpotify::Playlist.find(@user.id, params[:id])
+    @name = playlist.name
+    analyze_tracks(playlist.method(:tracks))
+    render :show_stats
+  end
+
+  private
+
+  def analyze_tracks(pager)
+    by_year = query_counts_by_year(pager)
     oldest, newest = by_year.keys.minmax
 
-    @year_labels = oldest..newest
+    @year_labels = newest.downto(oldest)
     @year_values = @year_labels.map { |year| by_year[year] || 0 }
   end
 
-  def query_counts_by_year
+  def query_counts_by_year(pager)
     by_year = Hash.new(0)
-    years = get_saved_tracks.inject([]) do |memo, track|
+    years = get_saved_tracks(pager).inject([]) do |memo, track|
       begin
         memo << Date.parse(track.album.release_date).year
       rescue Date::Error => e
@@ -27,17 +53,17 @@ class StatsController < ApplicationController
       by_year[year] += 1 
     end
 
-    by_year
+    return by_year
   end
 
-  def get_saved_tracks
+  def get_saved_tracks(pager)
     offset = 0
     limit = 20
     all = []
     while offset + limit < 1000
-      tracks = @user.saved_tracks(offset: offset, limit: limit)
-      break if tracks.empty? || tracks.size < limit
+      tracks = pager.call(offset: offset, limit: limit)
       all.concat(tracks)
+      break if tracks.empty? || tracks.size < limit
       offset += limit
     end
     return all
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'stats#index'
   get '/auth/spotify/callback', to: 'users#create'
+  
   get '/stats', to: 'stats#show'
+  get '/stats/saved', to: 'stats#show_saved_tracks'
+  get '/stats/playlist/:id', to: 'stats#show_playlist'
 end
